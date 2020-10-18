569f9bf61028fbe350fc7d737742676eadb76996
LaithRazeq <59575088+LaithRazeq@users.noreply.github.com>
2020-10-18 16:27:10 -0400

    Update CD.py

--- lab5/CD.py
+++ lab5/CD.py
@@ -1,9 +1,9 @@
 # SYSC3010 Lab 5 - CD.py
 #     The file to be jointly edited by Team Members C and D
-from C import * #laith is member C
+from C import * #laith is member C XD
 from D import * 
 def function_CD():
-    function_C() #laith added and created the function C
+    function_C() #laith added and created the function C XD
     function_D() 
     
 
