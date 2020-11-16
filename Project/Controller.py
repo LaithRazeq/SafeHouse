3944116a8b440c5cec2c04d4088626b077fb5122
AhmedAbdelrazik <AhmedAbdelrazik@cmail.carleton.ca>
2020-11-16 11:16:37 -0500

    automated testing added

--- Project/Controller.py
+++ Project/Controller.py
@@ -13,9 +13,14 @@ ID = 2
 
 READ_KEY="M8JC1XL6I75J08KL"
 WRITE_KEY="Y10RFX27NP5XFP5L"
+ARM=0
+LOCK=0
+CAM=0
+BUZZ=0
+MOTION=0
 
 
-def updateStatus():
+def updateStatus()->str:
     curr_status=read_data_thingspeak(READ_KEY)
     ARM=curr_status[0]
     LOCK=curr_status[1]
@@ -23,7 +28,8 @@ def updateStatus():
     BUZZ=curr_status[3]
     MOTION=curr_status[4]
     
-    print("System Status:\nArmed:"+ARM+", Door Locked:"+LOCK+", Camera:"+CAM+", Buzzer:"+BUZZ+", MOTION:"+MOTION)
+    status = ("System Status:\nArmed:"+ARM+", Door Locked:"+LOCK+", Camera:"+CAM+", Buzzer:"+BUZZ+", MOTION:"+MOTION+"\n")
+    return status
     
     
 def updateHistory():
@@ -34,3 +40,16 @@ def updateHistory():
         x-=1
         time.sleep(1)
 
+
+##Test function
+def test(desc:str, expected:str, actual:str)->str:
+    print(desc)
+    print("\nExpected result:\n", expected, "\nActual result:\n", actual)
+    if expected == actual:
+        print("Test passed")     
+    else:
+        print("Test failed") 
+
+##Main Script
+test('Testing updateStatus()','System Status:\nArmed:1, Door Locked:0, Camera:0, Buzzer:0, MOTION:0\n',updateStatus())
+updateHistory()
